18ef6dfca24148328abb5cf6cecb830593d1f428
Eduardo <edustartari@gmail.com>
2025-03-13 23:03:22 -0300

    testing deploy config for vercel

--- build_vercel.sh
+++ build_vercel.sh
@@ -1,4 +1,17 @@
 set -o errexit
 
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m virtualenv env
+
+# activate the virtual environment
+source env/bin/activate
+
+# install all deps in the venv
 pip install -r requirements.txt
+
+# collect static files using the Python interpreter from venv
 python manage.py collectstatic --noinput
+
+echo "BUILD END"
