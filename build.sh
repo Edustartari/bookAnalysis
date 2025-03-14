27e7b99e003cb431e6e84a38cb65a5fcdbe4d838
Eduardo <edustartari@gmail.com>
2025-03-13 23:14:12 -0300

    testing deploy config for vercel 4

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 set -o errexit
 
+echo "BUILD START"
+
+# create a virtual environment named 'venv' if it doesn't already exist
+python3.9 -m venv venv
+
+# activate the virtual environment
+source venv/bin/activate
+
 pip install -r requirements.txt
 
 cd frontend/
@@ -9,3 +17,5 @@ npm run build
 cd ..
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+echo "BUILD END"
