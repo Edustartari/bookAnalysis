9ada1991d72624d4c2e5efe9f1b60ee41709308a
Edustartari <edustartari@gmail.com>
2025-11-25 11:47:07 -0300

    fix lint error

--- frontend/js/App.js
+++ frontend/js/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { /* useState, */ useEffect } from 'react';
 import '/css/App.css';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
